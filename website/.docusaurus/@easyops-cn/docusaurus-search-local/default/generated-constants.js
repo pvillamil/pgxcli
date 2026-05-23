7369efe55fa639ac36b3179a45de0d0fe3cb6c8a
Balaji J <j.balaji2468@gmail.com>
2026-05-23 08:58:35 +0530

    rename website to docs
    Signed-off-by: Balaji J <j.balaji2468@gmail.com>

--- website/.docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
+++
@@ -1,5 +0,0 @@
-import lunr from "/home/balaji/projects/pgxcli/docs-site/node_modules/lunr/lunr.js";
-export const removeDefaultStopWordFilter = false;
-export const language = ["en"];
-export const searchIndexUrl = "search-index{dir}.json?_=12eb64b1";
-export const searchResultLimits = 8;
--- website/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/home/balaji/projects/pgxcli/docs-site/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/balaji/projects/pgxcli/docs-site/node_modules/infima/dist/css/default/default.css"),
-  require("/home/balaji/projects/pgxcli/docs-site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/balaji/projects/pgxcli/docs-site/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/balaji/projects/pgxcli/docs-site/src/css/custom.css"),
-  require("/home/balaji/projects/pgxcli/docs-site/src/theme/vercel-analytics.ts"),
-];
